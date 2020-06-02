e12f98d940239bcc90ebc4f36817c80e014a1fa8
Haroldsaayd <haroldalva98@gmail.com>
2020-06-02 18:40:20 -0500

    Se agrego la seccion de preguntas

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 
+
 $(".slideshow").slick({
   slidesToShow: 3,
   slidesToScroll: 1,
 });
+
+let mostrarRespuesta = document.getElementById("respuesta");
+let ocultarRespuesta = document.getElementById("pregunta");
+
+function respuesta() {
+
+  let estado = mostrarRespuesta.style.display
+  if (estado == "" || estado == "none") {
+    mostrarRespuesta.style.display = "grid";
+  } else {
+mostrarRespuesta.style.display = "none";
+  }
+  
+}
+
 
